0e9940449d612af9f64792cfda3631f4b15ef620
Yoo Daewhan <iok112@naver.com>
2026-04-05 00:42:03 +0900

    수정

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,29 +1,27 @@
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI as string
-
-if (!uri) {
-  throw new Error('MONGODB_URI 환경변수가 설정되지 않았습니다.')
-}
-
-let client: MongoClient
-let clientPromise: Promise<MongoClient>
-
 declare global {
   // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined
 }
 
-if (process.env.NODE_ENV === 'development') {
-  // 개발 환경: 핫 리로드 때 연결이 중복되지 않도록 global에 캐싱
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri)
-    global._mongoClientPromise = client.connect()
+function createClientPromise(): Promise<MongoClient> {
+  const uri = process.env.MONGODB_URI
+  if (!uri) {
+    // throw 대신 rejected Promise 를 반환 → 모듈 import 시 crash 없음
+    return Promise.reject(new Error('MONGODB_URI 환경변수가 설정되지 않았습니다.'))
   }
-  clientPromise = global._mongoClientPromise
-} else {
-  client = new MongoClient(uri)
-  clientPromise = client.connect()
+
+  if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+      global._mongoClientPromise = new MongoClient(uri).connect()
+    }
+    return global._mongoClientPromise
+  }
+
+  return new MongoClient(uri).connect()
 }
+
+const clientPromise: Promise<MongoClient> = createClientPromise()
 
 export default clientPromise
